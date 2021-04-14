aaf33eb23dfaeaf7eb1e1a1e5c20291406f80274
NagyIstvan <istvannagy8000@gmailcom>
2021-04-14 22:20:57 +0300

    Add project files.

---
+++ Migrations/20210414190122_InitialCreate.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectMovies.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movie",
+                columns: table => new
+                {
+                    ID = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    genre = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    star = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movie", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movie");
+        }
+    }
+}
---
+++ Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectMovies.Models
+{
+    public class Movie
+    {
+        public Guid ID { get; set; }
+        public string name { get; set; }
+        public string genre { get; set; }
+        public int star { get; set; }
+    }
+}
